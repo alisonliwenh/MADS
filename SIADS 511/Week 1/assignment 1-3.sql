cc0f56f9bb3eeef9b477ce7e1393891c83a23f3c
alisonggg <75323865+alisongh@users.noreply.github.com>
2021-05-04 20:31:18 -0400

    Update assignment 1-3.sql

--- SIADS 511/Week 1/assignment 1-3.sql
+++ SIADS 511/Week 1/assignment 1-3.sql
@@ -1,5 +1,12 @@
-create table automagic (
-  id SERIAL,
-  name VARCHAR(32),
-  height float(8) not NULL
+CREATE TABLE ages ( 
+  name VARCHAR(128), 
+  age INTEGER
 );
+
+DELETE FROM ages;
+
+INSERT INTO ages (name, age) VALUES ('Coleen', 30);
+INSERT INTO ages (name, age) VALUES ('Hyden', 37);
+INSERT INTO ages (name, age) VALUES ('Kenzii', 14);
+INSERT INTO ages (name, age) VALUES ('Shanea', 13);
+INSERT INTO ages (name, age) VALUES ('Vinh', 37);
