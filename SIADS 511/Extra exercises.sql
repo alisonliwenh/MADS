293c02f7cdb5a31db6c6c56f26dcaabc876b6c4b
alisonggg <75323865+alisongh@users.noreply.github.com>
2021-05-14 16:59:04 -0400

    Update Extra exercises.sql

--- SIADS 511/Extra exercises.sql
+++ SIADS 511/Extra exercises.sql
@@ -77,3 +77,39 @@ select mems.firstname || ' ' || mems.surname as member, faci.name as facility,
 	  (mems.memid != 0 and bks.slots*faci.membercost > 30)
 	)
 order by cost desc;
+
+-- How can you output a list of all members, including the individual who recommended them (if any), without using any joins? 
+-- Ensure that there are no duplicates in the list, and that each firstname + surname pairing is formatted as a column and ordered.
+select distinct mems.firstname || ' ' || mems.surname as member, (select recs.firstname || ' ' || recs.surname as recommender from cd.members recs where recs.memid=mems.recommendedby)
+from
+  cd.members mems
+order by member;
+
+-- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+-- Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. 
+-- Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost.
+select member, facility, cost from (
+	select 
+		mems.firstname || ' ' || mems.surname as member,
+		facs.name as facility,
+		case
+			when mems.memid = 0 then
+				bks.slots*facs.guestcost
+			else
+				bks.slots*facs.membercost
+		end as cost
+		from
+			cd.members mems
+			inner join cd.bookings bks
+				on mems.memid = bks.memid
+			inner join cd.facilities facs
+				on bks.facid = facs.facid
+		where
+			bks.starttime >= '2012-09-14' and
+			bks.starttime < '2012-09-15'
+	) as bookings
+	where cost > 30
+order by cost desc;  
+
+
+
