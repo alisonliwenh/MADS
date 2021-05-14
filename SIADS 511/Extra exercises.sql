905fee0c9d6886a635dc72eca47a25429d871f43
alisonggg <75323865+alisongh@users.noreply.github.com>
2021-05-14 16:46:11 -0400

    Update Extra exercises.sql

--- SIADS 511/Extra exercises.sql
+++ SIADS 511/Extra exercises.sql
@@ -33,3 +33,47 @@ select distinct recs.firstname as firstname, recs.surname as surname
 		inner join cd.members recs
 			on recs.memid = mems.recommendedby
 order by surname, firstname;      
+
+-- How can you output a list of all members, including the individual who recommended them (if any)? Ensure that results are ordered by (surname, firstname).
+select mems.firstname as memfname, mems.surname as memsname, recs.firstname as recfname, recs.surname as recsname
+	from 
+		cd.members mems
+		left outer join cd.members recs
+			on recs.memid = mems.recommendedby
+order by memsname, memfname;      
+
+-- How can you produce a list of all members who have used a tennis court? Include in your output the name of the court, and the name of the member formatted as a single column. 
+-- Ensure no duplicate data, and order by the member name followed by the facility name.
+select distinct mems.firstname || ' ' || mems.surname as member, faci.name as facility
+  from 
+    cd.members mems
+	inner join cd.bookings bks
+	  on mems.memid = bks.memid
+	inner join cd.facilities faci
+	   on bks.facid = faci.facid
+  where 
+    faci.name in ('Tennis Court 2','Tennis Court 1')
+order by member, facility;
+
+-- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. 
+-- Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost, and do not use any subqueries.
+select mems.firstname || ' ' || mems.surname as member, faci.name as facility,
+  case
+    when mems.memid=0 then
+	  bks.slots*faci.guestcost
+	else 
+	  bks.slots*faci.membercost
+  end as cost
+  from 
+    cd.members mems
+	  inner join cd.bookings bks
+	    on mems.memid=bks.memid
+	  inner join cd.facilities faci
+	    on bks.facid=faci.facid
+  where 
+    bks.starttime >= '2012-09-14' and
+	bks.starttime < '2012-09-15' and (
+	  (mems.memid = 0 and bks.slots*faci.guestcost > 30) or
+	  (mems.memid != 0 and bks.slots*faci.membercost > 30)
+	)
+order by cost desc;
