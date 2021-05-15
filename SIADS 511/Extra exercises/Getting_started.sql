e766b33d15db9bc9011095fca006183c90b41ee6
alisonggg <75323865+alisongh@users.noreply.github.com>
2021-05-14 23:12:41 -0400

    Update Getting_started.sql

--- SIADS 511/Extra exercises/Getting_started.sql
+++ SIADS 511/Extra exercises/Getting_started.sql
@@ -13,7 +13,8 @@
             REFERENCES cd.members(memid) ON DELETE SET NULL
     );
     
-    -- Each member has an ID (not guaranteed to be sequential), basic address information, a reference to the member that recommended them (if any), and a timestamp for when they joined. 
+    -- Each member has an ID (not guaranteed to be sequential), basic address information, a reference to the member that recommended them (if any), 
+    -- and a timestamp for when they joined. 
     -- The addresses in the dataset are entirely (and unrealistically) fabricated.
     CREATE TABLE cd.facilities
     (
@@ -26,7 +27,8 @@
        CONSTRAINT facilities_pk PRIMARY KEY (facid)
     );
     
-    -- The facilities table lists all the bookable facilities that the country club possesses. The club stores id/name information, the cost to book both members and guests, the initial cost to build the facility, and estimated monthly upkeep costs. 
+    -- The facilities table lists all the bookable facilities that the country club possesses. 
+    -- The club stores id/name information, the cost to book both members and guests, the initial cost to build the facility, and estimated monthly upkeep costs. 
     -- They hope to use this information to track how financially worthwhile each facility is.
     CREATE TABLE cd.bookings
     (
@@ -41,5 +43,7 @@
     );
           
     
--- Finally, there's a table tracking bookings of facilities. This stores the facility id, the member who made the booking, the start of the booking, and how many half hour 'slots' the booking was made for. 
--- This idiosyncratic design will make certain queries more difficult, but should provide you with some interesting challenges - as well as prepare you for the horror of working with some real-world databases :-).
+-- Finally, there's a table tracking bookings of facilities. This stores the facility id, the member who made the booking, the start of the booking, 
+-- and how many half hour 'slots' the booking was made for. 
+-- This idiosyncratic design will make certain queries more difficult, but should provide you with some interesting challenges 
+-- as well as prepare you for the horror of working with some real-world databases :-).
